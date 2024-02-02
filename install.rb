d9fef8d7f6004041112764ffb7b2ff4c82ff2d9b
Cairo Noleto <hey@caironoleto.dev>
2024-02-02 10:45:56 -0400

    feat: add supports to directories

--- install.rb
+++ install.rb
@@ -2,8 +2,9 @@
 
 home = File.expand_path('~')
 
-Dir['*'].each do |file|
+Dir['**/*'].each do |file|
   next if file =~ /install/
+  next if File.directory? file
 
   target = File.join(home, ".#{file}")
   `ln -nfs #{File.expand_path file} #{target}`
